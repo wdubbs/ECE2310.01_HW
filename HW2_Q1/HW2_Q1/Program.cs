50159e650424311cf3097b1ef2a199509a514b0e
wdubbs <56011301+wdubbs@users.noreply.github.com>
2020-09-06 23:51:14 -0700

    Adding HW2

---
+++ HW2_Q1/HW2_Q1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW2_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an integer: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            String N = "";
+            bool negative = false;
+
+            if (num < 0)
+            {
+                negative = true;
+                num *= -1;
+            }
+
+            while (num > 0)
+            {
+                N = Convert.ToString(num % 10) + "   " + N;
+                num /= 10;
+            }
+
+            if (negative)
+            {
+                N = "-   " + N;
+            }
+
+            Console.WriteLine(N);
+        }
+    }
+}
---
+++ HW2_Q2/HW2_Q2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW2_Q2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox3.ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e) //add
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double sum = num1 + num2;
+            textBox3.Text = $"{num1} + {num2} = {sum}";
+        }
+
+        private void button2_Click(object sender, EventArgs e) //subtract
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double sum = num1 - num2;
+            textBox3.Text = $"{num1} - {num2} = {sum}";
+        }
+
+        private void button3_Click(object sender, EventArgs e) //multiply
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double sum = num1 * num2;
+            textBox3.Text = $"{num1} * {num2} = {sum}";
+        }
+
+        private void button4_Click(object sender, EventArgs e) //average
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double sum = (num1 + num2)/2;
+            textBox3.Text = $"average of {num1} & {num2} is: {sum}";
+        }
+
+        private void button5_Click(object sender, EventArgs e) //clear
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e) //exit
+        {
+            Application.Exit();
+        }
+    }
+}
